476db3d0589c4c6b3adea582e5e5228bfd596d4f
Hoon Lee <yl.hoony@gmail.com>
2017-10-31 05:18:20 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,6 @@ def square_array(array)
   # your code here
   # array.collect { |e| e * e }
   squared_array = []
-  array.each do |e|
-    squared_array << e ** 2
-  end
+  array.each { |e| squared_array << e ** 2 }
   squared_array
 end
